bea69d1faed89a5f28ad5fafa11d780249e010b6
tc5v5s64ym-sketch <tc5v5s64ym@privaterelay.appleid.com>
2026-06-09 21:15:40 -0700

    Add sheet contract and catalog regression tests

--- config/sheetContract.js
+++ config/sheetContract.js
@@ -1,7 +1,29 @@
 const requiredSheetTabs = ['Metadata', 'Log_Cleaned', 'Exercise_Catalog', 'Effort', 'Logic', 'Session_Summary'];
 const optionalSheetTabs = ['Dashboard'];
 
+function getMissingRequiredTabs(tabNames) {
+  const present = new Set(tabNames || []);
+  return requiredSheetTabs.filter(tab => !present.has(tab));
+}
+
+function buildSheetContractStatus(tabNames) {
+  const present = new Set(tabNames || []);
+  return {
+    required: requiredSheetTabs.reduce((acc, tab) => {
+      acc[tab] = present.has(tab);
+      return acc;
+    }, {}),
+    optional: optionalSheetTabs.reduce((acc, tab) => {
+      acc[tab] = present.has(tab);
+      return acc;
+    }, {}),
+    missingRequiredTabs: getMissingRequiredTabs(tabNames)
+  };
+}
+
 module.exports = {
   requiredSheetTabs,
-  optionalSheetTabs
+  optionalSheetTabs,
+  getMissingRequiredTabs,
+  buildSheetContractStatus
 };
